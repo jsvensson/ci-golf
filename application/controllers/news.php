d6bd400b2c0dfe4fe2a9c84d56360eeb0ed80bb7
Johan Svensson <johan@atomicplayboy.net>
2012-08-18 13:55:46 +0200

    MVC for News

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -18,6 +18,32 @@ class News extends CI_Controller {
 		$this->load->view('templates/footer', $data);
 	}
 
+	public function create()
+	{
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
+		$data['title'] = 	'Create news item';
+
+		$this->form_validation->set_rules('title', 'Title', 'required');
+		$this->form_validation->set_rules('text', 'Text', 'required');
+
+		if ($this->form_validation->run() === FALSE)
+		{
+			$this->load->view('templates/header', $data);
+			$this->load->view('news/create');
+			$this->load->view('templates/footer');
+		}
+		else
+		{
+			$this->news_model->set_news();
+			$this->load->view('news/success');
+		}
+
+
+	}
+
+
 	public function view($slug)
 	{
 		$data['news'] = $this->news_model->get_news($slug);
