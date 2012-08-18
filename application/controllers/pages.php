5fe4fc31218fc695c1e6e15e5b506a0709374e78
Johan Svensson <johan@atomicplayboy.net>
2012-08-18 13:44:31 +0200

    Initial commit

---
+++ application/controllers/pages.php
@@ -0,0 +1,22 @@
+<?php 
+
+class Pages extends CI_Controller {
+
+	public function view($page = 'home')
+	{
+		if (!file_exists('application/views/pages/' . $page . '.php'))
+		{
+			// No such file, show 404
+			show_404();
+		}	
+
+		$data['title'] = ucfirst($page);
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('pages/' . $page, $data);
+		$this->load->view('templates/footer', $data);
+
+
+	}
+}
+?>
