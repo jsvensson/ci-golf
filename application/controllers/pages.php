b9adf13e4a20d5fc77cdf0894a4d480eea01574b
Johan Svensson <johan@atomicplayboy.net>
2012-08-28 11:31:14 +0200

    Better logging for Pages

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -7,7 +7,7 @@ class Pages extends CI_Controller {
 		if (!file_exists('application/views/pages/' . $page . '.php'))
 		{
 			// No such file, show 404
-			show_404();
+			show_404('Static page: ' . $page);
 		}	
 
 		$data['title'] = ucfirst($page);
