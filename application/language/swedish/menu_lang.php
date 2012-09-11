f8095a32bee743ce93ce327d3adae3ad6ecb759b
Johan Svensson <johan@atomicplayboy.net>
2012-09-11 10:37:22 +0200

    Initial Swedish lang file

---
+++ application/language/swedish/menu_lang.php
@@ -0,0 +1,17 @@
+<?php // Språkdefinition för menyn på svenska
+
+// Navbar items
+
+$lang['menu_item_brand']       = 'Golf';
+$lang['menu_item_news']        = 'Nyheter';
+$lang['menu_item_information'] = 'information';
+
+// Login box
+
+$lang['menu_login_email']      = 'Email';
+$lang['menu_login_password']   = 'Lösenord';
+$lang['menu_login_submit']     = 'Logga in';
+
+
+
+?>
