cd3480e159cf4d42ef278404501d1a0e4ae410d6
Johan Svensson <johan@atomicplayboy.net>
2012-09-18 12:56:00 +0200

    User signup form

--- application/views/user/join.php
+++ application/views/user/join.php
@@ -7,3 +7,18 @@ $this->load->helper('form');
 <h3>Registrera nytt konto</h3>
 
 <?= form_open('user/create') ?>
+
+<label for="email"><?= $this->lang->line('form_label_email') ?></label>
+<input type="text" name="email" placeholder="<?= $this->lang->line('form_placeholder_email') ?>">
+
+<label for="password"><?= $this->lang->line('form_label_password') ?></label>
+<input type="password" name="password" placeholder="<?= $this->lang->line('form_placeholder_password') ?>">
+
+<label for="password"><?= $this->lang->line('form_label_password_repeat') ?></label>
+<input type="password" name="password2" placeholder="<?= $this->lang->line('form_placeholder_password') ?>">
+
+<br>
+
+<button type="submit" class="btn">Registrera konto</button>
+
+<?= form_close() ?>
