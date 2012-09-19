65212e5e34b7b576e68ad2f2589b53a52de3a0e3
Johan Svensson <johan@atomicplayboy.net>
2012-09-19 13:11:11 +0200

    Basic user list

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,8 +1,21 @@
 	<h3>Användare</h3>
 
-	<ul>
-<?php foreach ($user as $user_item): ?>
-		<li><?= $user_item['username'] ?></li>
 
+	<table>
+		<tr>
+			<th>ID</th>
+			<th>Namn</th>
+			<th>Email</th>
+			<th>Status</th>
+			<th>Nivå</th>
+		</tr>
+<?php foreach ($user as $user_item): ?>
+		<tr>
+			<td><?= $user_item['id'] ?></td>
+			<td><?= $user_item['fullname'] ?></td>
+			<td><?= $user_item['username'] ?></td>
+			<td><?= $user_item['status'] ?></td>
+			<td><?= $user_item['level'] ?></td>
+		</tr>
 <?php endforeach ?>
-	</ul>
+	</table>
