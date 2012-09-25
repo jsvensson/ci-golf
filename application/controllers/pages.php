1dab20abec6e21cf28afbdcdbc917b044367fb6c
Johan Svensson <johan@atomicplayboy.net>
2012-09-25 18:53:39 +0200

    Ugly hack for static page titles

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -10,10 +10,16 @@ class Pages extends CI_Controller {
 		{
 			// No such file, show 404
 			show_404('Static page: ' . $page);
-		}	
+		}
+
+		// FIXME: Ugly hack for static page titles
+		$titles = array(
+			'index' => 'Golf - framsidan',
+			'about' => 'Golf - Information'
+		);
 
 		$this->data['subview'] = 'pages/' . $page;
-		$this->data['title'] = ucfirst($page);
+		$this->data['title'] = $titles[$page];
 		$this->load->view('layouts/default', $this->data);
 	}
 }
