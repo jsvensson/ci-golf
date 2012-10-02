799bc955b0f2bd1b4bc8d6da33aff5fbe7087006
Johan Svensson <johan@atomicplayboy.net>
2012-10-02 11:09:54 +0200

    Load pw helper in constructor

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -5,6 +5,7 @@ class User_model extends CI_Model {
 	public function __construct()
 	{
 		$this->load->database();
+		$this->load->helper('password');
 	}
 
 	public function get_user($id = FALSE)
@@ -25,7 +26,6 @@ class User_model extends CI_Model {
 
 	public function set_user()
 	{
-		$this->load->helper('password');
 
 		// Salt and encrypt password (SHA1)
 		$salted_pw = sha1($this->input->post('password') . password_salt());
