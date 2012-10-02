d657a360d0ef1c4cec622d5d3a60be47a886c9a6
Johan Svensson <johan@atomicplayboy.net>
2012-10-02 11:26:42 +0200

    Field names

--- application/views/partials/header_menu_loginform.php
+++ application/views/partials/header_menu_loginform.php
@@ -1,6 +1,6 @@
 				<?= form_open('user/login', array('class' => 'navbar-form pull-right')) ?>
 
-					<input class="span2" type="text" placeholder="<?= $this->lang->line('form_placeholder_email') ?>">
-					<input class="span2" type="password" placeholder="<?= $this->lang->line('form_placeholder_password') ?>">
+					<input class="span2" type="text" name="username" placeholder="<?= $this->lang->line('form_placeholder_email') ?>">
+					<input class="span2" type="password" name="password" placeholder="<?= $this->lang->line('form_placeholder_password') ?>">
 					<button type="submit" class="btn"><?= $this->lang->line('form_login_button') ?></button>
 				<?= form_close(); ?>
