14201f727eb189f35f5835f4933f246f74645568
Johan Svensson <johan@atomicplayboy.net>
2012-10-09 13:55:52 +0200

    User#settings view

---
+++ application/views/user/settings.php
@@ -0,0 +1,28 @@
+<h2>Inställningar</h2>
+
+<?= validation_errors() ?>
+
+<?= form_open('user/settings') ?>
+
+<label for="username"><?= $this->lang->line('form_label_email') ?></label>
+<input type="text" name="username" placeholder="<?= $this->lang->line('form_placeholder_email') ?>" value="<?= $this->data['user']->username ?>">
+
+<label for="firstname"><?= $this->lang->line('form_label_firstname') ?></label>
+<input type="text" name="firstname" placeholder="<?= $this->lang->line('form_placeholder_firstname') ?>" value="<?= $this->data['user']->firstname ?>">
+
+<label for="lastname"><?= $this->lang->line('form_label_lastname') ?></label>
+<input type="text" name="lastname" placeholder="<?= $this->lang->line('form_placeholder_lastname') ?>" value="<?= $this->data['user']->lastname ?>">
+
+<label for="password"><?= $this->lang->line('form_label_password') ?></label>
+<input type="password" name="password" placeholder="<?= $this->lang->line('form_placeholder_password') ?>" value="<?= set_value('password'); ?>">
+
+<label for="password"><?= $this->lang->line('form_label_password_repeat') ?></label>
+<input type="password" name="verify_password" placeholder="<?= $this->lang->line('form_placeholder_password') ?>" value="<?= set_value('verify_password'); ?>">
+
+<br>
+
+<button type="submit" class="btn">Spara inställningar</button>
+
+<?= form_close() ?>
+
+<?php $this->dbug->inspect($this->data['user']); ?>
