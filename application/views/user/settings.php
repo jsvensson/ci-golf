a9369a8cc9602dfa858ba8996168b88b583722fd
Johan Svensson <johan@atomicplayboy.net>
2012-10-22 13:36:53 +0200

    Settings view: validation errors

--- application/views/user/settings.php
+++ application/views/user/settings.php
@@ -1,22 +1,25 @@
 <h2>Inställningar</h2>
 
-<?= validation_errors() ?>
-
 <?= form_open('user/settings') ?>
 
 <label for="username"><?= $this->lang->line('form_label_email') ?></label>
+<?= form_error('username'); ?>
 <input type="text" name="username" placeholder="<?= $this->lang->line('form_placeholder_email') ?>" value="<?= $this->data['user']->username ?>">
 
 <label for="firstname"><?= $this->lang->line('form_label_firstname') ?></label>
+<?= form_error('firstname'); ?>
 <input type="text" name="firstname" placeholder="<?= $this->lang->line('form_placeholder_firstname') ?>" value="<?= $this->data['user']->firstname ?>">
 
 <label for="lastname"><?= $this->lang->line('form_label_lastname') ?></label>
+<?= form_error('lastname'); ?>
 <input type="text" name="lastname" placeholder="<?= $this->lang->line('form_placeholder_lastname') ?>" value="<?= $this->data['user']->lastname ?>">
 
 <label for="password"><?= $this->lang->line('form_label_password') ?></label>
+<?= form_error('password'); ?>
 <input type="password" name="password" placeholder="<?= $this->lang->line('form_placeholder_password') ?>" value="<?= set_value('password'); ?>">
 
 <label for="password"><?= $this->lang->line('form_label_password_repeat') ?></label>
+<?= form_error('verify_password'); ?>
 <input type="password" name="verify_password" placeholder="<?= $this->lang->line('form_placeholder_password') ?>" value="<?= set_value('verify_password'); ?>">
 
 <br>
