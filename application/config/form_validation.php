484b52aa5644ab9811af1b2c08dc5525079d96c7
Johan Svensson <johan@atomicplayboy.net>
2012-10-30 14:37:59 +0100

    Standalone form validation config

---
+++ application/config/form_validation.php
@@ -0,0 +1,40 @@
+<?php
+
+$config = array(
+	'signup' => array(
+		array(
+			'field' => 'username',
+			'label' => 'lang:form_name_username',
+			'rules' => 'trim|required|is_unique[user.username]|valid_email'
+		),
+		array(
+			'field' => 'password',
+			'label' => 'lang:form_name_password',
+			'rules' => 'required|min_length[6]|matches[verify_password]'
+		),
+		array(
+			'field' => 'verify_password',
+			'label' => 'lang:form_name_verify_password',
+			'rules' => 'required'
+		)
+	),
+	'user_settings' => array(
+		array(
+			'field' => 'username',
+			'label' => 'lang:form_name_username',
+			'rules' => 'trim|required|valid_email'
+		),
+		array(
+			'field' => 'password',
+			'label' => 'lang:form_name_password',
+			'rules' => 'required|min_length[6]|matches[verify_password]'
+		),
+		array(
+			'field' => 'verify_password',
+			'label' => 'lang:form_name_verify_password',
+			'rules' => 'required'
+		)
+	)
+);
+
+/* EOF */
