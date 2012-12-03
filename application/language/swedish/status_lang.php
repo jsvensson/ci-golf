e3c6437599abb7b25fa716fa7b409ffd7de248d4
Johan Svensson <johan@atomicplayboy.net>
2012-12-03 14:24:36 +0100

    User level lang change

--- application/language/swedish/status_lang.php
+++ application/language/swedish/status_lang.php
@@ -5,7 +5,7 @@ $lang['user_active_status_0'] = 'Inaktiv';
 $lang['user_active_status_1'] = 'Aktiv';
 
 // User level names
-$lang['user_level_0'] = 'Användare';
+$lang['user_level_1'] = 'Användare';
 $lang['user_level_5'] = 'Administratör';
 
 
