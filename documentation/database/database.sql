507c920e17f7619a81eb509e0b25d632e797f5d7
Johan Svensson <johan@atomicplayboy.net>
2012-12-10 14:44:13 +0100

    Database snapshot

--- documentation/database/database.sql
+++ documentation/database/database.sql
@@ -34,6 +34,16 @@ CREATE TABLE `ci_sessions` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `ci_sessions`
+--
+
+LOCK TABLES `ci_sessions` WRITE;
+/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
+INSERT INTO `ci_sessions` VALUES ('1b3c53e9ff5d00d1e8d36c0ae8416ec9','127.0.0.1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',1355141062,''),('5b0bdb64075cba18d02adaede5ceace2','127.0.0.1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',1354049691,''),('b4c8a409ac2b61bc46d84467bf0c258f','127.0.0.1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',1354541280,'a:3:{s:7:\"user_id\";s:1:\"1\";s:11:\"login_state\";b:1;s:10:\"user_level\";s:1:\"5\";}'),('c272f02d92a2490838b1def9bf1ecdc7','127.0.0.1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',1354030929,'a:3:{s:7:\"user_id\";s:1:\"1\";s:11:\"login_state\";b:1;s:10:\"user_level\";s:1:\"5\";}'),('fdfa0d78c0cfbc0a05ab38abe653d482','127.0.0.1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11',1354654290,'');
+/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `group`
 --
 
@@ -48,6 +58,15 @@ CREATE TABLE `group` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `group`
+--
+
+LOCK TABLES `group` WRITE;
+/*!40000 ALTER TABLE `group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `groups_users`
 --
 
@@ -60,6 +79,15 @@ CREATE TABLE `groups_users` (
   PRIMARY KEY (`group_id`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups_users`
+--
+
+LOCK TABLES `groups_users` WRITE;
+/*!40000 ALTER TABLE `groups_users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `groups_users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `news`
@@ -77,6 +105,16 @@ CREATE TABLE `news` (
   KEY `slug` (`slug`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `news`
+--
+
+LOCK TABLES `news` WRITE;
+/*!40000 ALTER TABLE `news` DISABLE KEYS */;
+INSERT INTO `news` VALUES (1,'Första nyheten','first-post','En hög med text'),(2,'Mera nyheter','mera-nyheter','Något slags innehåll.'),(3,'Nyheter','nyheter','test');
+/*!40000 ALTER TABLE `news` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -97,6 +135,16 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'johan@atomicplayboy.net','Johan','Svensson',1,5,'39488d3ed32f7c3e19fdaf3ba4afd03787b9aeb7',0),(59,'test@test.se','Bob','Hund',0,1,'39488d3ed32f7c3e19fdaf3ba4afd03787b9aeb7',0),(62,'test2@test.se','Bob','Katt',1,1,'39488d3ed32f7c3e19fdaf3ba4afd03787b9aeb7',0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -107,4 +155,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-12-10 14:03:12
+-- Dump completed on 2012-12-10 14:07:42
