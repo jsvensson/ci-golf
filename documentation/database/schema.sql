43218a81a5f6552ffb6d44c857fbe65d109900ad
Johan Svensson <johan@atomicplayboy.net>
2012-12-19 15:20:32 +0100

    Schema rename
    Table Group -> Groups

--- documentation/database/schema.sql
+++ documentation/database/schema.sql
@@ -19,10 +19,10 @@
 -- Table structure for table `Group`
 --
 
-DROP TABLE IF EXISTS `Group`;
+DROP TABLE IF EXISTS `Groups`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Group` (
+CREATE TABLE `Groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(40) DEFAULT 'Ny grupp',
   PRIMARY KEY (`id`)
@@ -65,7 +65,7 @@ CREATE TABLE `Score` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `group_id` (`group_id`),
-  CONSTRAINT `FK_Group_Score` FOREIGN KEY (`group_id`) REFERENCES `Group` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `FK_Groups_Score` FOREIGN KEY (`group_id`) REFERENCES `Groups` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `FK_User_Score` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -87,24 +87,24 @@ CREATE TABLE `User` (
   `password` char(40) NOT NULL DEFAULT '',
   `handicap` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `UserGroup`
 --
 
-DROP TABLE IF EXISTS `UserGroup`;
+DROP TABLE IF EXISTS `UserGroups`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `UserGroup` (
+CREATE TABLE `UserGroups` (
   `group_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   PRIMARY KEY (`group_id`,`user_id`),
   KEY `group_id_idx` (`group_id`),
   KEY `user_id_idx` (`user_id`),
-  CONSTRAINT `FK_UserGroup_Group_group_id` FOREIGN KEY (`group_id`) REFERENCES `Group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_UserGroup_User_user_id` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `FK_UserGroups_Groups_group_id` FOREIGN KEY (`group_id`) REFERENCES `Groups` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `FK_UserGroups_User_user_id` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
