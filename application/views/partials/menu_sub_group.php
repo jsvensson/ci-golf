7046b5adee03dfe9963a34bcbc04ace4b989dc3d
Johan Svensson <johan@atomicplayboy.net>
2013-01-08 11:07:58 +0100

    Show group link

--- application/views/partials/menu_sub_group.php
+++ application/views/partials/menu_sub_group.php
@@ -1,4 +1,4 @@
 <div class="btn-group">
-	<button class="btn"><i class="icon-list"></i> Visa grupper</button>
+	<button class="btn"><i class="icon-list"></i> <?= anchor('groups', 'Visa grupper') ?></button>
 	<button class="btn"><i class="icon-plus"></i> Skapa grupp</button>
 </div>
