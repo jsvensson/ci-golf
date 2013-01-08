87db51b6ad4a45afd2ff0ec87b32321c13608ff2
Johan Svensson <johan@atomicplayboy.net>
2013-01-08 10:50:14 +0100

    Move Bootstrap/jQuery JS files to subfolders in assets

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -31,8 +31,8 @@ if (isset($this->data['submenu'])): ?>
 	</div><!-- end .container -->
 
 <!-- Load javascript -->
-    <script type="text/javascript" src="<?= base_url(); ?>assets/js/jquery.js"></script>
-    <script type="text/javascript" src="<?= base_url(); ?>assets/js/bootstrap.js"></script>
+    <script type="text/javascript" src="<?= base_url(); ?>assets/js/jquery/jquery.js"></script>
+    <script type="text/javascript" src="<?= base_url(); ?>assets/js/bootstrap/bootstrap.js"></script>
 
 </body>
 </html>
