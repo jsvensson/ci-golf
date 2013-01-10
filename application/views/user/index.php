cac98dcfa1267c0e05ee8fcb364c401fc23a3c26
Johan Svensson <johan@atomicplayboy.net>
2013-01-10 12:21:06 +0100

    User link goes to non-admin User#view

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -17,7 +17,7 @@
 		<tr>
 			<td><input type="checkbox"></td>
 			<td><?= $user['id'] ?></td>
-			<td><a href="<?= base_url() . 'admin/user/' . $user['id'] ?>"><?= $user['fullname'] ?></a></td>
+			<td><a href="<?= base_url() . 'user/' . $user['id'] ?>"><?= $user['fullname'] ?></a></td>
 			<td><?= $user['username'] ?></td>
 			<td class="user status _<?= $user['active'] ?>"><?= $this->lang->line('user_active_status_' . $user['active']) ?></td>
 			<td class="user level _<?= $user['level'] ?>"><?= $user['level'] ?> &ndash; <?= $this->lang->line('user_level_' . $user['level']) ?></td>
