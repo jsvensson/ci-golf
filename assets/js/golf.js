16637bf9db4252d104c08b88b421590d2e9dcbcd
Johan Svensson <johan@atomicplayboy.net>
2013-01-11 16:55:26 +0100

    Menu well animation speed
    Now that the damn animation bug is nailed.

--- assets/js/golf.js
+++ assets/js/golf.js
@@ -1,9 +1,9 @@
 // Open a target well
 $('.btn-open-well').click( function() {
-	$('#' + $(this).data('openref')).slideDown(500);
+	$('#' + $(this).data('openref')).slideDown(250);
 });
 
 // Close a target well
 $('.btn-close-well').click( function() {
-	$('#' + $(this).data('closeref')).slideUp(500);
+	$('#' + $(this).data('closeref')).slideUp(250);
 });
