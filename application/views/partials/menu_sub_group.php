6ae1bdec4394d2342fef76919a6bcf3eef7f6532
Johan Svensson <johan@atomicplayboy.net>
2013-01-14 13:21:41 +0100

    Menu well subgroup

--- application/views/partials/menu_sub_group.php
+++ application/views/partials/menu_sub_group.php
@@ -1,49 +1,53 @@
-<div class="btn-group">
-	<a class="btn" href="<?= base_url(); ?>groups/"><i class="icon-list"></i> Visa grupper</a>
+<div class="btn-group btn-group-submenu">
+	<a class="btn" href="<?= url_to('group/list') ?>"><i class="icon-list"></i> Visa grupper</a>
 	<a class="btn btn-open-well" data-openref="well_add_group"><i class="icon-plus"></i> Skapa grupp</a>
 </div>
 
-<div class="well well-edit well-submenu attr-hidden" id="well_add_group">
-	<h4>Skapa grupp</h4>
+<div class="well-container">
 
-	<?= form_open('group/create', array('class' => 'form-horizontal')) ?>
-	<form class="form-horizontal" method="post">
+	<div class="well well-edit well-submenu attr-hidden" id="well_add_group">
+		<h4>Skapa grupp</h4>
 
-		<div class="control-group">
-			<label class="control-label">Namn</label>
-			<div class="controls">
-				<input type="text" placeholder="Namn">
+		<?= form_open('group/create', array('class' => 'form-horizontal')) ?>
+		<form class="form-horizontal" method="post">
+
+			<div class="control-group">
+				<label class="control-label">Namn</label>
+				<div class="controls">
+					<input type="text" name="group_name" placeholder="Namn">
+				</div>
 			</div>
-		</div>
 
-		<div class="control-group">
-			<label class="control-label">Grupptyp</label>
-			<div class="controls">
-				<label class="radio">
-					<input type="radio" name="show_membership" value="public" checked>Öppen grupp (alla kan gå med)
-				</label>
+			<div class="control-group">
+				<label class="control-label">Grupptyp</label>
+				<div class="controls">
+					<label class="radio">
+						<input type="radio" name="show_membership" value="public" checked>Öppen grupp (alla kan gå med)
+					</label>
 
-				<label class="radio">
-					<input type="radio" name="show_membership" value="hidden">Privat grupp (inbjudan krävs)
-				</label>
+					<label class="radio">
+						<input type="radio" name="show_membership" value="hidden">Privat grupp (inbjudan krävs)
+					</label>
+				</div>
 			</div>
-		</div>
 
-		<div class="control-group">
-			<label class="control-label">Resultat</label>
-			<div class="controls">
-				<label class="checkbox">
-					<input type="checkbox" name="public_scores" value="on" checked>Offentliga gruppresultat (icke-medlemmar kan se gruppens poäng)
-				</label>
+			<div class="control-group">
+				<label class="control-label">Resultat</label>
+				<div class="controls">
+					<label class="checkbox">
+						<input type="checkbox" name="public_scores" value="on" checked>Offentliga gruppresultat (icke-medlemmar kan se gruppens poäng)
+					</label>
+				</div>
 			</div>
-		</div>
 
-		<div class="control-group">
-			<div class="controls">
-				<button class="btn btn-cancel btn-close-well" type="button" data-closeref="well_add_group">Avbryt</button>
-				<button class="btn btn-info" type="submit"><i class="icon-plus icon-white"></i> Skapa grupp</button>
+			<div class="control-group">
+				<div class="controls">
+					<button class="btn btn-cancel btn-close-well" type="button" data-closeref="well_add_group">Avbryt</button>
+					<button class="btn btn-info" type="submit"><i class="icon-plus icon-white"></i> Skapa grupp</button>
+				</div>
 			</div>
-		</div>
 
-	<?= form_close(); ?>
-</div>
+		<?= form_close(); ?>
+	</div><!-- end .well-submenu -->
+
+</div><!-- end .well-container -->
