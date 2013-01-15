34a95c2da523d21e88dff80ddfd32776ff2b4d4f
Johan Svensson <johan@atomicplayboy.net>
2013-01-15 11:18:30 +0100

    Group#list edit button

--- application/views/group/list.php
+++ application/views/group/list.php
@@ -3,6 +3,7 @@
 	<table class="table table-condensed table-hover">
 		<thead>
 			<tr>
+				<th></th>
 				<th>ID</th>
 				<th>Namn</th>
 				<th>Medlemmar</th>
@@ -10,6 +11,11 @@
 	</thead>
 <?php foreach ($grouplist as $group): ?>
 		<tr>
+			<td>
+				<div class="btn-group">
+					<a href="<?= url_to('group/edit/' . $group['id']); ?>" class="btn btn-mini" title="Redigera grupp"><i class="icon-edit"></i></a>
+				</div>
+			</td>
 			<td><?= $group['id'] ?></td>
 			<td><?= $group['name'] ?></td>
 			<td><?= $group['member_count'] ?></td>
