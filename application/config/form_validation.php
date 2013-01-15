031c0c0ee2b6cedf3fde79923faae920329393e4
Johan Svensson <johan@atomicplayboy.net>
2013-01-15 11:17:09 +0100

    Trim fields in validation instead of model

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -9,6 +9,16 @@ $config = array(
 			'rules' => 'trim|required|is_unique[User.username]|valid_email'
 		),
 		array(
+			'field' => 'firstname',
+			'label' => 'lang:form_name_firstname',
+			'rules' => 'trim'
+		),
+		array(
+			'field' => 'lastname',
+			'label' => 'lang:form_name_lastname',
+			'rules' => 'trim'
+		),
+		array(
 			'field' => 'password',
 			'label' => 'lang:form_name_password',
 			'rules' => 'required|min_length[6]|matches[verify_password]'
@@ -26,6 +36,16 @@ $config = array(
 			'field' => 'username',
 			'label' => 'lang:form_name_username',
 			'rules' => 'trim|required|valid_email'
+		),
+		array(
+			'field' => 'firstname',
+			'label' => 'lang:form_name_firstname',
+			'rules' => 'trim'
+		),
+		array(
+			'field' => 'lastname',
+			'label' => 'lang:form_name_lastname',
+			'rules' => 'trim'
 		),
 		array(
 			'field' => 'password',
