64391efe591c34f6a49fa7ca5738ec65a6870a14
Johan Svensson <johan@atomicplayboy.net>
2013-01-30 21:26:10 +0100

    Basic group member view

--- application/views/group/show.php
+++ application/views/group/show.php
@@ -1,3 +1,11 @@
 <h3>Visar grupp <?= $this->data['group']->id ?></h3>
 
 <?php $this->dbug->inspect($this->data['group']); ?>
+
+<h3>Medlemmar</h3>
+
+<?php foreach ($group_members as $member): ?>
+
+<?= $member['username'] ?><br>
+
+<?php endforeach ?>
