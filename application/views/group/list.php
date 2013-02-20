1f2ec76cf5f636c7393a7240675a279ff91a06fb
Johan Svensson <johan@atomicplayboy.net>
2013-02-20 22:57:27 +0100

    Group#list view

--- application/views/group/list.php
+++ application/views/group/list.php
@@ -13,12 +13,12 @@
 		<tr>
 			<td>
 				<div class="btn-group">
-					<a href="<?= url_to('group/edit/' . $group['id']); ?>" class="btn btn-mini" title="Redigera grupp"><i class="icon-edit"></i></a>
+					<a href="<?= url_to('group/show', $group->id) ?>" class="btn btn-mini" title="Redigera grupp"><i class="icon-edit"></i></a>
 				</div>
 			</td>
-			<td><?= $group['id'] ?></td>
-			<td><a href="<?= url_to('group/show/' . $group['id']); ?>"><?= $group['name'] ?></a></td>
-			<td><?= $group['member_count'] ?></td>
+			<td><?= $group->id ?></td>
+			<td><a href="<?= $group->id ?>"><?= $group->name ?></a></td>
+			<td><?= $group->member_count ?></td>
 		</tr>
 <?php endforeach ?>
 	</table>
